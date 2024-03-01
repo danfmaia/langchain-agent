0a61be243fca85195686a621697382dd85e82b53
Dan Maia <danilofmaia@gmail.com>
2024-03-01 14:49:18 -0300

    dev: stable point

--- test_2.py
+++ test_2.py
@@ -2,18 +2,37 @@ from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts import MessagesPlaceholder
+
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
+
 from langchain.chains import create_history_aware_retriever
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.vectorstores import FAISS  # pylint: disable=E0611
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+llm = ChatOpenAI()
+
+llm.invoke("how can langsmith help with testing?")
+
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are world class technical documentation writer."),
+#     ("user", "{input}")
+# ])
+
+# output_parser = StrOutputParser()
+
+# chain = prompt | llm | output_parser
+
+# response = chain.invoke({"input": "how can langsmith help with testing?"})
+# print(response)
 
 loader = WebBaseLoader("https://docs.smith.langchain.com")
-
 
 docs = loader.load()
 
@@ -23,11 +42,6 @@ text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(docs)
 vector = FAISS.from_documents(documents, embeddings)
 
-# prompt = ChatPromptTemplate.from_messages([
-#     ("system", "You are world class technical documentation writer."),
-#     ("user", "{input}")
-# ])
-
 # prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
 
 # <context>
@@ -36,31 +50,23 @@ vector = FAISS.from_documents(documents, embeddings)
 
 # Question: {input}""")
 
+# document_chain = create_stuff_documents_chain(llm, prompt)
+
+retriever = vector.as_retriever()
+# retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+# response = retrieval_chain.invoke(
+#     {"input": "how can langsmith help with testing?"})
+# print(response["answer"])
+
+# First we need a prompt that we can pass into an LLM to generate this search query
+
 prompt = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name="chat_history"),
     ("user", "{input}"),
     ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
 ])
-
-
-llm = ChatOpenAI()
-
-output_parser = StrOutputParser()
-
-# chain = prompt | llm | output_parser
-
-# response = chain.invoke({"input": "how can langsmith help with testing?"})
-# print(response)
-
-document_chain = create_stuff_documents_chain(llm, prompt)
-
-retriever = vector.as_retriever()
-# retrieval_chain = create_retrieval_chain(retriever, document_chain)
 retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-
-# response = retrieval_chain.invoke(
-#     {"input": "how can langsmith help with testing?"})
-# print(response["answer"])
 
 chat_history = [HumanMessage(
     content="Can LangSmith help test my LLM applications?"), AIMessage(content="Yes!")]
@@ -68,4 +74,26 @@ response = retriever_chain.invoke({
     "chat_history": chat_history,
     "input": "Tell me how"
 })
+print()
+print(response)
+print()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system",
+     "Answer the user's questions based on the below context:\n\n{context}"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+])
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
+
+chat_history = [HumanMessage(
+    content="Can LangSmith help test my LLM applications?"), AIMessage(content="Yes!")]
+response = retrieval_chain.invoke({
+    "chat_history": chat_history,
+    "input": "Tell me how"
+})
+print()
 print(response["answer"])
+print()
